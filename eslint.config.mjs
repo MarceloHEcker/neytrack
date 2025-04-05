12b5d2d0a470895e2576f5fad953648c59ff2214
Marcelo Ecker <marcelohhecker@gmail.com>
2025-04-04 22:29:09 -0300

    chore: improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,42 +2,34 @@ import { defineConfig } from 'eslint/config'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
 })
 
-export default defineConfig([{
-    extends: compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
-
-    plugins: {
-        '@typescript-eslint': typescriptEslint,
-    },
-
-    languageOptions: {
-        globals: {
-            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, 'off'])),
-            ...globals.node,
+export default defineConfig([
+    {
+        extends: compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
         },
-
-        parser: tsParser,
-        ecmaVersion: 12,
-        sourceType: 'module',
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+            parser: tsParser,
+            ecmaVersion: 2021,
+            sourceType: 'module',
+        },
+        rules: {
+            semi: ['error', 'never'],
+            quotes: ['error', 'single'],
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
     },
-
-    rules: {
-        semi: ['error', 'never'],
-        quotes: ['error', 'single'],
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'error',
-        '@typescript-eslint/no-explicit-any': 'warn',
-    },
-}])
+])
